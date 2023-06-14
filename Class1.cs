a51bad699a6fedb09912cf2dd3bb8e5c301f4768
Roman Pyatnov <romaandbears@gmail.com>
2023-06-14 13:38:59 +0300

    1

---
+++ Class1.cs
@@ -0,0 +1,151 @@
+﻿public abstract class Transport
+{
+    protected string _name { get; set; }
+    protected int _year { get; set; }
+    protected int _weight { get; set; }
+    protected string _color { get; set; }
+
+
+    public Transport() { }
+
+    public Transport(string _name, int year, int weight, string color)
+    {
+        this._name = _name;
+        this._year = year;
+        this._weight = weight;
+        this._color = color;
+    }
+
+    public abstract void Info();
+}
+class Train : Transport
+{
+    protected int _carriages { get; set; }
+    public Train(string _name, int _year, int _weight, string _color, int carriages) : base(_name, _year, _weight, _color)
+    {
+        this._carriages = carriages;
+    }
+
+    public override void Info()
+    {
+        Console.WriteLine("Train");
+        Console.WriteLine($"Name: {_name}\n" +
+                          $"Year: {_year}\n" +
+                          $"Weight: {_weight}\n" +
+                          $"Color: {_color}");
+        Console.WriteLine($"Сarriages: {_carriages}\n");
+    }
+}
+public class Airplane : Transport
+{
+    protected double _wingLength { get; set; }
+    public Airplane(string _name, int _year, int _weight, string _color, double wingLength) : base(_name, _year, _weight, _color)
+    {
+        this._wingLength = wingLength;
+    }
+
+    public override void Info()
+    {
+        Console.WriteLine("Airplane");
+        Console.WriteLine($"Name: {_name}\n" +
+                          $"Year: {_year}\n" +
+                          $"Weight: {_weight}\n" +
+                          $"Color: {_color}");
+        Console.WriteLine($"WingLength: {_wingLength:0.00}\n");
+    }
+}
+public class Car : Transport
+{
+    protected double _speed { get; set; }
+    public Car(string _name, int _year, int _weight, string _color, double speed) : base(_name, _year, _weight, _color)
+    {
+        this._speed = speed;
+    }
+
+    public override void Info()
+    {
+        Console.WriteLine("Car");
+        Console.WriteLine($"Name: {_name}\n" +
+                          $"Year: {_year}\n" +
+                          $"Weight: {_weight}\n" +
+                          $"Color: {_color}");
+        Console.WriteLine($"Speed: {_speed:0.00}\n");
+    }
+}
+public class Truck : Car
+{
+    protected double _bodyLength { get; set; }
+    public Truck(string _name, int _year, int _weight, string _color, double _speed, double bodyLength) : base(_name, _year, _weight, _color, _speed)
+    {
+        this._bodyLength = bodyLength;
+    }
+
+    public override void Info()
+    {
+        Console.WriteLine("Truck");
+        Console.WriteLine($"Name: {_name}\n" +
+                          $"Year: {_year}\n" +
+                          $"Weight: {_weight}\n" +
+                          $"Color: {_color}\n" +
+                          $"Speed: {_speed:0.00}");
+        Console.WriteLine($"BodyLength: {_bodyLength:0.00}\n");
+    }
+}
+public class Passenger : Car
+{
+    protected string _passengerType { get; set; }
+    public Passenger(string _name, int _year, int _weight, string _color, double _speed, string passengerType) : base(_name, _year, _weight, _color, _speed)
+    {
+        this._passengerType = passengerType;
+    }
+
+    public override void Info()
+    {
+        Console.WriteLine("Truck");
+        Console.WriteLine($"Name: {_name}\n" +
+                          $"Year: {_year}\n" +
+                          $"Weight: {_weight}\n" +
+                          $"Color: {_color}\n" +
+                          $"Speed: {_speed:0.00}");
+        Console.WriteLine($"PassengerType: {_passengerType}\n");
+    }
+}
+public class PassengerPlane : Airplane
+{
+    protected int _seats { get; set; }
+    public PassengerPlane(string _name, int _year, int _weight, string _color, double _wingLength, int seats) : base(_name, _year, _weight, _color, _wingLength)
+    {
+        this._seats = seats;
+    }
+
+    public override void Info()
+    {
+        Console.WriteLine("Airplane");
+        Console.WriteLine($"Name: {_name}\n" +
+                          $"Year: {_year}\n" +
+                          $"Weight: {_weight}\n" +
+                          $"Color: {_color}\n" +
+                          $"WingLength: {_wingLength:0.00}");
+        Console.WriteLine($"Seats: {_seats}\n");
+    }
+}
+public class CargoPlane : Airplane
+{
+    protected double _capacity { get; set; }
+
+    public CargoPlane(string _name, int _year, int _weight, string _color, double _wingLength, double capacity) : base(_name, _year, _weight, _color, _wingLength)
+    {
+        this._capacity = capacity;
+    }
+
+    public override void Info()
+    {
+        Console.WriteLine("Cargo Airplane");
+        Console.WriteLine($"Name: {_name}\n" +
+                          $"Year: {_year}\n" +
+                          $"Weight: {_weight}\n" +
+                          $"Color: {_color}\n" +
+                          $"WingLength: {_wingLength:0.00}");
+        Console.WriteLine($"Capacity: {_capacity:0.00}\n");
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿CargoPlane Plane1 = new CargoPlane("Plane1", 1982, 150000, "Blue", 30, 500);
+CargoPlane Plane2 = new CargoPlane("Plane2", 1999, 100000, "Green", 20, 200);
+
+PassengerPlane IL62 = new PassengerPlane("IL62", 1966, 70400, "White", 43.2, 186);
+PassengerPlane IL86 = new PassengerPlane("IL86", 1967, 113950, "White-Blue", 48.1, 350);
+
+Car Subaru = new Car("Subaru", 2000, 3, "Black", 250);
+Car Toyota = new Car("Toyota", 1980, 3, "White", 280);
+
+Truck Ford = new Truck("Ford", 2000, 4, "Orange", 200, 5);
+Truck Ram = new Truck("Ram", 2010, 4, "Gray", 180, 4);
+
+Train Train1 = new Train("Train1", 1950, 200000, "Mixed", 420);
+Train Train2 = new Train("Train2", 1920, 300000, "Black", 300);
+
+Plane1.Info();
+Plane2.Info();
+IL62.Info();
+IL86.Info();
+Subaru.Info();
+Toyota.Info();
+Ford.Info();
+Ram.Info();
+Train1.Info();
+Train2.Info();
